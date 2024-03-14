5c3e261e432f75b2fc9d9d40990faf3ea6928592
Ethan-Leone-9134 <130795044+Ethan-Leone-9134@users.noreply.github.com>
2024-03-14 13:08:21 -0400

    Save changes to pygame libraries and add graphing

--- pyGames/colors.py
+++ pyGames/colors.py
@@ -1,4 +1,6 @@
-
+"""
+Package includes various color triples
+"""
 
 # Grey Scale
 WHITE = (255, 255, 255)
@@ -6,7 +8,7 @@ BLACK = (0, 0, 0)
 GRAY = (128, 128, 128)
 LIGHTGRAY = (195, 195, 195)
 
-# Super Basic
+# Super Basic 255s
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
@@ -14,20 +16,24 @@ YELLOW = (255, 255, 0)
 CYAN = (0, 255, 255)
 MAGENTA = (255, 0, 255)
 
-# Additional Colors
-ORANGE = (255, 165, 0)
+# Mildly Basic 128s
+MAROON = (128, 0, 0)
+LIME = (0, 128, 0)
+NAVY = (0, 0, 128)
+OLIVE = (128, 128, 0)
+TEAL = (0, 128, 128)
 PURPLE = (128, 0, 128)
-PINK = (255, 192, 203)
+
+# Additional Colors
+INDIGO = (75, 0, 130)
 BROWN = (165, 42, 42)
 GOLD = (255, 215, 0)
 SILVER = (192, 192, 192)
-NAVY = (0, 0, 128)
-TEAL = (0, 128, 128)
-OLIVE = (128, 128, 0)
-LIME = (0, 128, 0)
-MAROON = (128, 0, 0)
-INDIGO = (75, 0, 130)
+ORANGE = (255, 165, 0)
+PINK = (255, 192, 203)
 LAVENDER = (230, 230, 250)
 
 # Transparent Placeholder
 CLEAR = (0, 0, 0, 0)
+
+#%% End of Package File
